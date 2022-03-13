822007d17a0e6ce0e2bdfcacd6c995532ab5a1b9
basdawimp <basharoldknight@gmail.com>
2022-03-13 21:28:22 +0000

    Tests for individual methods

--- src/sheet/sheet.controller.spec.ts
+++ src/sheet/sheet.controller.spec.ts
@@ -1,18 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SheetController } from './sheet.controller';
+import { SheetService } from './sheet.service';
 
 describe('SheetController', () => {
   let controller: SheetController;
+  let service: SheetService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SheetController],
+      providers: [SheetService],
     }).compile();
 
+    service = module.get<SheetService>(SheetService);
     controller = module.get<SheetController>(SheetController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('getStats', () => {
+    it('should return a long sting of stats', async () => {
+      let result: 'test';
+      jest.spyOn(service, 'getStats').mockImplementation(() => result);
+    });
   });
 });
--- src/sheet/sheet.service.spec.ts
+++ src/sheet/sheet.service.spec.ts
@@ -15,4 +15,12 @@ describe('SheetService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('getStats', () => {
+    it('returns correct stats', async () => {
+      expect(service.getStats()).toEqual(
+        `name: one\nstrength: 0\nagility: 0\nfighting: 0\nawareness: 0\nstamina: 0\ndexterity: 0\ninterllect: 0\n\ndodge: 0\nparry: 0\nfortitude: 0\ntoughness: 0\nwill: 0\n\nacrobatics: 0\nathletics: 0\ncloseCombat: 0\ndeception: 0\nexpertise: 0\ninsight: 0\nintimidation: 0\ninvestigation: 0\nperception: 0\npersuasion: 0\nrangedCombat: 0\nslightOfHand: 0\nstealth: 0\ntechnology: 0\ntreatment: 0\nvehicles: 0`,
+      );
+    });
+  });
 });
