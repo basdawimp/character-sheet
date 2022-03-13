c276ad3f7ec10b1f5ad13a617043cd0a8d03ff02
basdawimp <basharoldknight@gmail.com>
2022-03-13 19:57:31 +0000

    Removed code for a fresh start.

--- src/sheet/sheet.controller.ts
+++ src/sheet/sheet.controller.ts
@@ -1,22 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { SheetService } from './sheet.service';
+import { Controller } from '@nestjs/common';
 
 @Controller('sheet')
-export class SheetController {
-  constructor(private readonly sheetService: SheetService) {}
-
-  @Post()
-  makeNewSheet(@Body('name') name: string, @Body('stats') stats: number[]) {
-    this.sheetService.makeNewSheet(name, stats);
-  }
-
-  @Get()
-  getAllSheets() {
-    return this.sheetService.getSheets();
-  }
-
-  @Get(':name')
-  getSheetByName(@Param(':name') sheetName: string) {
-    return this.sheetService.getSheetByName(sheetName);
-  }
-}
+export class SheetController {}
--- src/sheet/sheet.service.ts
+++ src/sheet/sheet.service.ts
@@ -1,19 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { Sheet } from './sheet.class';
 
 @Injectable()
-export class SheetService {
-  private sheets: Sheet[] = [];
-
-  makeNewSheet(name, stats: number[]) {
-    this.sheets.push(new Sheet(name, stats));
-  }
-
-  getSheets() {
-    return this.sheets;
-  }
-
-  getSheetByName(sheetName: string) {
-    return sheetName;
-  }
-}
+export class SheetService {}
