43f04d1b3f3c3c61268dc9c14c19f0f8e2ef8c0b
basdawimp <basharoldknight@gmail.com>
2022-03-14 22:17:34 +0000

    Minimum code to pass test.
    Had the getStats function return desired result.

--- src/sheet/sheet.service.ts
+++ src/sheet/sheet.service.ts
@@ -7,7 +7,38 @@ export class SheetService {
   }
 
   getStats() {
+    return {
+      strength: 0,
+      agility: 0,
+      fighting: 0,
+      awareness: 0,
+      stamina: 0,
+      dexterity: 0,
+      interllect: 0,
 
+      dodge: 0,
+      parry: 0,
+      fortitude: 0,
+      toughness: 0,
+      will: 0,
+
+      acrobatics: 0,
+      athletics: 0,
+      closeCombat: 0,
+      deception: 0,
+      expertise: 0,
+      insight: 0,
+      intimidation: 0,
+      investigation: 0,
+      perception: 0,
+      persuasion: 0,
+      rangedCombat: 0,
+      slightOfHand: 0,
+      stealth: 0,
+      technology: 0,
+      treatment: 0,
+      vehicles: 0,
+    };
   }
 
   getStatsByProfile(profileName) {
