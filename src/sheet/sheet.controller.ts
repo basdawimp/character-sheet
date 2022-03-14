c13d6be8290408e6630deb45dccc368fed01f21c
basdawimp <basharoldknight@gmail.com>
2022-03-14 21:13:24 +0000

    innitial creation of intended methods.
    Set up the minimum methods I intend to use. This is subject to change.

--- src/sheet/sheet.controller.ts
+++ src/sheet/sheet.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { SheetService } from './sheet.service';
 
 @Controller('sheet')
 export class SheetController {
   constructor(private readonly sheetService: SheetService) {}
 
+  setStats() {
+    return this.sheetService.setStats();
+  }
+
   @Get()
-  getStats(): string {
+  getStats() {
     return this.sheetService.getStats();
+  }
+
+  @Get(':profile')
+  getStatsByProfile(@Param('profile') profileName) {
+    return this.sheetService.getStatsByProfile(profileName);
+  }
+
+  setProfile() {
+    return this.setProfile();
+  }
+
+  getProfile() {
+    return this.sheetService.getProfile();
   }
 }
--- src/sheet/sheet.service.ts
+++ src/sheet/sheet.service.ts
@@ -2,5 +2,23 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class SheetService {
+  setStats() {
+
+  }
+
+  getStats() {
+
+  }
+
+  getStatsByProfile(profileName) {
+
+  }
+
+  setProfile() {
+
+  }
+
+  getProfile() {
+
   }
 }
