c5d907e8b33ea0a22a327cb6bb72d64718bdf444
basdawimp <basharoldknight@gmail.com>
2022-03-15 21:41:26 +0000

    condenced class.

--- src/sheet/sheet.class.ts
+++ src/sheet/sheet.class.ts
@@ -1,5 +1,4 @@
 export class Sheet {
-  name: string;
   strength: number;
   agility: number;
   fighting: number;
@@ -30,72 +29,4 @@ export class Sheet {
   technology: number;
   treatment: number;
   vehicles: number;
-
-  constructor(
-    name: string,
-    [
-      strength,
-      agility,
-      fighting,
-      awareness,
-      stamina,
-      dexterity,
-      interllect,
-
-      dodge,
-      parry,
-      fortitude,
-      toughness,
-      will,
-
-      acrobatics,
-      athletics,
-      closeCombat,
-      deception,
-      expertise,
-      insight,
-      intimidation,
-      investigation,
-      perception,
-      persuasion,
-      rangedCombat,
-      slightOfHand,
-      stealth,
-      technology,
-      treatment,
-      vehicles,
-    ]: number[],
-  ) {
-    this.name = name;
-    this.strength = strength;
-    this.agility = agility;
-    this.fighting = fighting;
-    this.awareness = awareness;
-    this.stamina = stamina;
-    this.dexterity = dexterity;
-    this.interllect = interllect;
-
-    this.dodge = dodge;
-    this.parry = parry;
-    this.fortitude = fortitude;
-    this.toughness = toughness;
-    this.will = will;
-
-    this.acrobatics = acrobatics;
-    this.athletics = athletics;
-    this.closeCombat = closeCombat;
-    this.deception = deception;
-    this.expertise = expertise;
-    this.insight = insight;
-    this.intimidation = intimidation;
-    this.investigation = investigation;
-    this.perception = perception;
-    this.persuasion = persuasion;
-    this.rangedCombat = rangedCombat;
-    this.slightOfHand = slightOfHand;
-    this.stealth = stealth;
-    this.technology = technology;
-    this.treatment = treatment;
-    this.vehicles = vehicles;
-  }
 }
